385c2f071a6c15843f24735ae84b41a2a326b91b
mionki <piousmionki@gmail.com>
2022-08-26 02:33:25 +0300

    31 avl insert

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -5,7 +5,7 @@
  * @tree: pointer to the root node of the tree
  *
  * Return: 1 if tree is avl
- *         0 otherwise
+ *         else return 0
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
@@ -50,6 +50,7 @@ int btia_helper(const binary_tree_t *tree, int min, int max)
  *
  * Return: height of the tree
  *         0 if tree is NULL
+ * 
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
